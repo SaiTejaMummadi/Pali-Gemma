c07437a76a2ae68d8d083e10fb71a1e23ff14916
shponglese <saionthemail@gmail.com>
2024-12-24 13:07:00 -0800

    inference

---
+++ utils.py
@@ -0,0 +1,8 @@
+import glob
+from safetensors import safe_open
+from typing import Tuple
+import os
+
+def load_hf_model(model_path: str, device: str) -> Tuple[PaliGemmaForConditionalGeneration, AutoTokenizer]:
+    #Load the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_path, padding)
